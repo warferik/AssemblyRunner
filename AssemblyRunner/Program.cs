fbcc655b4cba40229821efe7fdf092ded29c99fb
warferik <erik.warfield@gmail.com>
2018-10-17 08:48:30 -0600

    Update Program.cs
    Added Sandbox checks

--- AssemblyRunner/Program.cs
+++ AssemblyRunner/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
 using System.IO;
 using System.Net;
+using System.Diagnostics;
 
 
 namespace AssemblyRunner
@@ -80,6 +81,58 @@ namespace AssemblyRunner
                 return bytes;
             }
         }
+
+        public static bool SBOX()
+        {
+            bool sbox = false;
+            List<string> EOfS = new List<string>();
+            string[] FilePaths = {@"C:\windows\Sysnative\Drivers\Vmmouse.sys",
+            @"C:\windows\Sysnative\Drivers\vm3dgl.dll", @"C:\windows\Sysnative\Drivers\vmdum.dll",
+            @"C:\windows\Sysnative\Drivers\vm3dver.dll", @"C:\windows\Sysnative\Drivers\vmtray.dll",
+            @"C:\windows\Sysnative\Drivers\vmci.sys", @"C:\windows\Sysnative\Drivers\vmusbmouse.sys",
+            @"C:\windows\Sysnative\Drivers\vmx_svga.sys", @"C:\windows\Sysnative\Drivers\vmxnet.sys",
+            @"C:\windows\Sysnative\Drivers\VMToolsHook.dll", @"C:\windows\Sysnative\Drivers\vmhgfs.dll",
+            @"C:\windows\Sysnative\Drivers\vmmousever.dll", @"C:\windows\Sysnative\Drivers\vmGuestLib.dll",
+            @"C:\windows\Sysnative\Drivers\VmGuestLibJava.dll", @"C:\windows\Sysnative\Drivers\vmscsi.sys",
+            @"C:\windows\Sysnative\Drivers\VBoxMouse.sys", @"C:\windows\Sysnative\Drivers\VBoxGuest.sys",
+            @"C:\windows\Sysnative\Drivers\VBoxSF.sys", @"C:\windows\Sysnative\Drivers\VBoxVideo.sys",
+            @"C:\windows\Sysnative\vboxdisp.dll", @"C:\windows\Sysnative\vboxhook.dll",
+            @"C:\windows\Sysnative\vboxmrxnp.dll", @"C:\windows\Sysnative\vboxogl.dll",
+            @"C:\windows\Sysnative\vboxoglarrayspu.dll", @"C:\windows\Sysnative\vboxoglcrutil.dll",
+            @"C:\windows\Sysnative\vboxoglerrorspu.dll", @"C:\windows\Sysnative\vboxoglfeedbackspu.dll",
+            @"C:\windows\Sysnative\vboxoglpackspu.dll", @"C:\windows\Sysnative\vboxoglpassthroughspu.dll",
+            @"C:\windows\Sysnative\vboxservice.exe", @"C:\windows\Sysnative\vboxtray.exe",
+            @"C:\windows\Sysnative\VBoxControl.exe"};
+            foreach (string FilePath in FilePaths)
+            {
+                if (File.Exists(FilePath))
+                {
+                    EOfS.Add(FilePath);
+                }
+            }
+            int test = EOfS.Count;
+            if (TimeZone.CurrentTimeZone.StandardName == "Coordinated Universal Time")
+            {   
+                test += 1; 
+            }
+
+            PerformanceCounter pc = new PerformanceCounter("System", "System Up Time");
+            pc.NextValue();
+            int uptime = (int)pc.NextValue();
+
+            if (uptime / 3600 < 1)
+            {
+                test += 1;
+            }
+
+            if (test >= 1 || EOfS.Count >=1)
+            {
+                sbox = true;
+            }
+
+
+            return sbox;
+        }
     }
 
     class Program
@@ -88,6 +141,12 @@ namespace AssemblyRunner
 
         static void Main(string[] args)
         {
+            bool stest = Shared.SBOX();
+            if (stest == true)
+            {
+                Console.WriteLine("Ahh, Ahh, Ahh, you didnt say the magic word");
+                return;
+            }
             if (args.Length < 2 || args.Length > 4)
             {
                 Console.WriteLine("");
@@ -221,4 +280,5 @@ namespace AssemblyRunner
          }
     }
 }
+ 
  
