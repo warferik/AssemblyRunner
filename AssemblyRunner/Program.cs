70922b5a690002651a86a21d89ababf37293d21a
warferik <erik.warfield@gmail.com>
2018-10-17 10:32:56 -0600

    Update Program.cs

--- AssemblyRunner/Program.cs
+++ AssemblyRunner/Program.cs
@@ -70,32 +70,8 @@ namespace AssemblyRunner
         public static bool SBOX()
         {
             bool sbox = false;
-            List<string> EOfS = new List<string>();
-            string[] FilePaths = {@"C:\windows\Sysnative\Drivers\Vmmouse.sys",
-            @"C:\windows\Sysnative\Drivers\vm3dgl.dll", @"C:\windows\Sysnative\Drivers\vmdum.dll",
-            @"C:\windows\Sysnative\Drivers\vm3dver.dll", @"C:\windows\Sysnative\Drivers\vmtray.dll",
-            @"C:\windows\Sysnative\Drivers\vmci.sys", @"C:\windows\Sysnative\Drivers\vmusbmouse.sys",
-            @"C:\windows\Sysnative\Drivers\vmx_svga.sys", @"C:\windows\Sysnative\Drivers\vmxnet.sys",
-            @"C:\windows\Sysnative\Drivers\VMToolsHook.dll", @"C:\windows\Sysnative\Drivers\vmhgfs.dll",
-            @"C:\windows\Sysnative\Drivers\vmmousever.dll", @"C:\windows\Sysnative\Drivers\vmGuestLib.dll",
-            @"C:\windows\Sysnative\Drivers\VmGuestLibJava.dll", @"C:\windows\Sysnative\Drivers\vmscsi.sys",
-            @"C:\windows\Sysnative\Drivers\VBoxMouse.sys", @"C:\windows\Sysnative\Drivers\VBoxGuest.sys",
-            @"C:\windows\Sysnative\Drivers\VBoxSF.sys", @"C:\windows\Sysnative\Drivers\VBoxVideo.sys",
-            @"C:\windows\Sysnative\vboxdisp.dll", @"C:\windows\Sysnative\vboxhook.dll",
-            @"C:\windows\Sysnative\vboxmrxnp.dll", @"C:\windows\Sysnative\vboxogl.dll",
-            @"C:\windows\Sysnative\vboxoglarrayspu.dll", @"C:\windows\Sysnative\vboxoglcrutil.dll",
-            @"C:\windows\Sysnative\vboxoglerrorspu.dll", @"C:\windows\Sysnative\vboxoglfeedbackspu.dll",
-            @"C:\windows\Sysnative\vboxoglpackspu.dll", @"C:\windows\Sysnative\vboxoglpassthroughspu.dll",
-            @"C:\windows\Sysnative\vboxservice.exe", @"C:\windows\Sysnative\vboxtray.exe",
-            @"C:\windows\Sysnative\VBoxControl.exe"};
-            foreach (string FilePath in FilePaths)
-            {
-                if (File.Exists(FilePath))
-                {
-                    EOfS.Add(FilePath);
-                }
-            }
-            int test = EOfS.Count;
+            
+            int test = 0;
             if (TimeZone.CurrentTimeZone.StandardName == "Coordinated Universal Time")
             {   
                 test += 1; 
@@ -110,7 +86,7 @@ namespace AssemblyRunner
                 test += 1;
             }
 
-            if (test >= 1 || EOfS.Count >=1)
+            if (test >=1)
             {
                 sbox = true;
             }
